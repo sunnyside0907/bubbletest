d360ce16c79b3fcfa46cb29e7b97f9a3346338da
채혜진 <sunnyside0907@bubblecon.io>
2020-07-10 15:50:17 +0900

    첨부파일 업데이트 수정

--- pyboard/board/views.py
+++ pyboard/board/views.py
@@ -157,9 +157,11 @@ def update(request):
 
     # select * from board_board where idx=id
     dto_src = Board.objects.get(idx=id)
+
+    dto_src.save()
     
     fname = dto_src.filename  # 기존 첨부파일 이름
-    fsize = 0   # 기존 첨부파일 크기
+    fsize = dto_src.filesize   # 기존 첨부파일 크기
 
     if "file" in request.FILES:        # 새로운 첨부파일이 있으면
         file = request.FILES["file"]
@@ -177,7 +179,7 @@ def update(request):
 
         # 수정 후 board의 내용
     dto_new = Board(idx=id, writer=request.POST["writer"], title=request.POST["title"],
-                    content=request.POST["content"], filename=fname, filesize=fsize)
+                    content=request.POST["content"], filename=fname, filesize=fsize )
     dto_new.save()      # update query 호출
         
     return redirect("/")        # 시작페이지로 이ddong
