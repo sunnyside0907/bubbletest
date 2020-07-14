3d6d7936f251b5acc5c70b89590008444e94f71e
채혜진 <sunnyside0907@bubblecon.io>
2020-07-14 15:36:48 +0900

    pagination 1/3page 이런식으로 완료

--- pyboard/board/views.py
+++ pyboard/board/views.py
@@ -1,20 +1,26 @@
 import os
 import math
-
+import socket
+from datetime import datetime
 from django.http.response import HttpResponse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.http import urlquote
 from django.views.decorators.csrf import csrf_exempt
 from board.models import Board, Comment     #model.py에 있는 테이블 사용
 from django.db.models import Q              #Q()| 사용
+from django.core.paginator import *
+from . import models
+from django.views.generic import ListView
 
 
 UPLOAD_DIR = "C:/upload/" # upload 폴더정보
 
 # Create your views here.
 
+
 # 글 읽기 페이지 작성
+@csrf_exempt
 def list(request):
     try:
         search_option = request.POST["search_option"]
@@ -33,7 +39,7 @@ def list(request):
         boardCount = Board.objects.filter(writer__contains = search).count()
     elif search_option == "title":
         boardCount = Board.objects.filter(title__contains = search).count()
-    elif search_option == "contetn":
+    elif search_option == "content":
         boardCount = Board.objects.filter(content__contains = search).count()
     else:
         boardCount = Board.objects.all().count()
@@ -44,23 +50,9 @@ def list(request):
         start = 0
 
     page_size = 10
-    block_size =10
+    block_size = 10
     end = start+page_size
-    total_page = math.ceil(boardCount/page_size)
-    current_page = math.ceil((start+1)/page_size)
-    start_page = math.floor((current_page-1)/block_size)*block_size+1
-    end_page = start_page+block_size-1
 
-    if total_page < end_page:
-        end_page = total_page
-    if start_page >= block_size:
-        prev_list =(start_page-2)*page_size
-    else:
-        prev_list = 0
-    if end_page < total_page:
-        next_list = end_page*page_size
-    else:
-        next_list = 0
 
     if search_option == "all":
         boardList = Board.objects.filter(
@@ -75,20 +67,31 @@ def list(request):
     else:
         boardList = Board.objects.all().order_by('-idx')[start:end]
 
-    links=[]
-    for i in range(start_page, end_page+1):
-        page_start = (i-1)*page_size
-        links.append("<a href='?start="+str(page_start)+"'>"+str(i)+"</a>")
 
+    # 페이지 네이션
+    boardList = Board.objects.all().order_by("-idx")
+    paginator = Paginator(boardList,10)
+    try:
+        page = request.GET.get("page")
+    except:
+        page = 1
+    try:
+        boardList = paginator.page(page)
+    except PageNotAnInteger:
+        boardList = paginator.page(1)
+    except EmptyPage:
+        boardList = paginator.page(paginator.num_pages)
+
+
+    
     return render(request, "list.html",
                   {"boardList":boardList, "boardCount":boardCount, "search_option":search_option, "search":search,
-                   "range":range(start_page-1,end_page),"start_page":start_page,"end_page":end_page,
-                   "block_size":block_size, "total_page":total_page, "prev_list":prev_list,"next_list":next_list,
-                   "link":links,
+                                      
                    }
                   )
-#fileter 는 where Q() 는 %% like 검색
-  
+#fileter  where Q() 는 %% like 검색
+
+
 
 # 글쓰기 페이지 작성
 def write(request):
@@ -212,3 +215,7 @@ def reply_insert(request):
 
     # detai?idx=글번호 페이지로 이동
     return HttpResponseRedirect("detail?idx="+id)
+
+
+
+
